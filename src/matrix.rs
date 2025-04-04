157bec7d80f93f84782f9b4f02685bbd16a3c488
Di <dihuynh2302@gmail.com>
2025-04-04 14:36:57 -0400

    bug fix

--- src/matrix.rs
+++ src/matrix.rs
@@ -8,10 +8,10 @@ pub struct Matrix<T: Copy + Clone + Default> {
     r: u32,
     c: u32,
     e: Vec<Row<T>>,
-    i: u32
+    i: u32,
 }
 
-impl <'a, T: 'a + Copy + Clone + Default> Matrix<T> {
+impl<'a, T: 'a + Copy + Clone + Default> Matrix<T> {
     pub fn new(r: u32, c: u32) -> Self {
         let mut e = vec![];
         for _ in 0..r {
@@ -22,7 +22,7 @@ impl <'a, T: 'a + Copy + Clone + Default> Matrix<T> {
 
     pub fn fill<IT: Iterator<Item = &'a T>>(&mut self, mut it: IT) {
         for i in 0..self.r * self.c {
-            let r = (i / self.r) as usize;
+            let r = (i / self.c) as usize;
             let c = (i % self.c) as usize;
             self[r][c] = *it.next().unwrap();
         }
@@ -33,7 +33,7 @@ impl <'a, T: 'a + Copy + Clone + Default> Matrix<T> {
     }
 }
 
-impl <T: Copy + Clone + Default> Index<usize> for Matrix<T> {
+impl<T: Copy + Clone + Default> Index<usize> for Matrix<T> {
     type Output = Row<T>;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -41,13 +41,13 @@ impl <T: Copy + Clone + Default> Index<usize> for Matrix<T> {
     }
 }
 
-impl <T: Copy + Clone + Default> IndexMut<usize> for Matrix<T> {
+impl<T: Copy + Clone + Default> IndexMut<usize> for Matrix<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.e[index]
     }
 }
 
-impl <T: Copy + Clone + Default> Index<usize> for Row<T> {
+impl<T: Copy + Clone + Default> Index<usize> for Row<T> {
     type Output = T;
 
     fn index(&self, index: usize) -> &Self::Output {
@@ -55,13 +55,13 @@ impl <T: Copy + Clone + Default> Index<usize> for Row<T> {
     }
 }
 
-impl <T: Copy + Clone + Default> IndexMut<usize> for Row<T> {
+impl<T: Copy + Clone + Default> IndexMut<usize> for Row<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.0[index]
     }
 }
 
-impl <T: Copy + Clone + Default + Mul<T, Output = T> + Add<Output = T>> Mul for Matrix<T> {
+impl<T: Copy + Clone + Default + Mul<T, Output = T> + Add<Output = T>> Mul for Matrix<T> {
     type Output = Matrix<T>;
 
     fn mul(self, b: Self) -> Self::Output {
@@ -73,7 +73,8 @@ impl <T: Copy + Clone + Default + Mul<T, Output = T> + Add<Output = T>> Mul for 
             let j = idx % b.c;
 
             for k in 0..a.c {
-                c[i as usize][j as usize] = c[i as usize][j as usize] + a[i as usize][k as usize] * b[k as usize][j as usize];
+                c[i as usize][j as usize] = c[i as usize][j as usize]
+                    + a[i as usize][k as usize] * b[k as usize][j as usize];
             }
         }
 
@@ -81,7 +82,7 @@ impl <T: Copy + Clone + Default + Mul<T, Output = T> + Add<Output = T>> Mul for 
     }
 }
 
-impl <T: Copy + Clone + Default> Iterator for Matrix<T> {
+impl<T: Copy + Clone + Default> Iterator for Matrix<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
